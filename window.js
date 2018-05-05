3d98387b6a50d062d806d49f20d5a0374d805422
mayankmetha <mayankmethad@gmail.com>
2018-05-05 15:53:46 +0530

    init

---
+++ window.js
@@ -0,0 +1,27 @@
+$(() => {
+  const CryptoJS = require('crypto-js');
+  const fs = require('fs');
+  var uid = "";
+  var ele = "";
+  var mac = fs.readFileSync('blockchain.key').toString();
+  $('#username').bind('input propertychange', function() {
+    onUsernameInput(this.value);
+  });
+  $('#election').bind('input propertychange', function() {
+    onElectionInput(this.value)
+  });
+  $('#username').focus();
+  function onUsernameInput(text) {
+    const tmpuid = CryptoJS.SHA512(text).toString();
+    const uidhash = CryptoJS.HmacSHA512(tmpuid,mac).toString();
+    uid = uidhash;
+    $('#uid').text(uidhash);
+    const blkhash = CryptoJS.SHA256(uid+ele).toString();
+    $('#blk').text(blkhash);
+  }
+  function onElectionInput(text) {
+    ele = text;
+    const blkhash = CryptoJS.SHA256(uid+ele).toString();
+    $('#blk').text(blkhash);
+  }
+});
